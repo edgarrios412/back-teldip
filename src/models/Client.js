8998d1eefbe6d4e6ddb5cbb6c0c30ac2c4fb76f0
unknown <edgarrios412@gmail.com>
2023-11-12 11:25:04 -0500

    Add model compromiso

--- src/models/Client.js
+++ src/models/Client.js
@@ -6,6 +6,10 @@ module.exports = (sequelize) => {
       primaryKey:true,
       autoIncrement:true,
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
     cedula: {
       type: DataTypes.STRING,
       allowNull: true,

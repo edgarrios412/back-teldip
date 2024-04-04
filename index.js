44fae8546d96f014c1e002c9181f6ee33f563e79
edgarrios412 <edgarrios412@gmail.com>
2024-04-04 10:55:20 -0500

    add .env to gitignore

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require("./src/app")
 const {conn} = require("./src/db")
+const PORT = 3001 || process.env.PORT
 
 conn.sync({alter:true}).then(() => {
     console.log("Conectado a la base de datos")
-    app.listen(3001, () => {
+    app.listen(PORT, () => {
         console.log("Servidor en linea en el puerto 3001")
     })
 })

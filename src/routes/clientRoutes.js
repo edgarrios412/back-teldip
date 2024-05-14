1cbebb2bb0ac6b07512d8beeb3ff65c62bea368d
edgarrios412 <edgarrios412@gmail.com>
2024-05-14 15:03:52 -0500

    fix

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,21 +1,7 @@
 const { Router } = require("express");
-const upload = require("../helpers/multer");
 const clientRoutes = Router();
 const path = require("node:path");
 const { createUserLog } = require("../controllers/userLogController");
-
-clientRoutes.get("/test", async (req, res) => {
-  // await createUserLog(req, "Ingreso a test");
-  res.json({status:true});
-});
-
-clientRoutes.post("/test", upload.single("file"), async (req, res) => {
-  // await createUserLog(req, "Subio el archivo " + req.file.originalname);
-  console.log(req.file)
-  // const pdf = await pdfApi.uploadFile("test", req.file.buffer)
-  // console.log(pdf)
-  res.json(req.file);
-});
 
 clientRoutes.get("/files/:filename", async (req, res) => {
   createUserLog(req, "Miro el archivo " + req.params.filename)

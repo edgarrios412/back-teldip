f119a143ce34792b3175343ec5251353664a9c39
edgarrios412 <edgarrios412@gmail.com>
2024-05-22 15:18:44 -0500

    push

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express()
 const indexRoutes = require("./routes/index")
 const cors = require("cors")
 const morgan = require("morgan")
+const cron = require("node-cron")
+const axios = require("axios")
+
+cron.schedule("*/30 * * * * *", () => {
+    axios.get("https://gptfree-dv8p.onrender.com/activate").then(() => console.log("Activado"))
+})
 
 app.use('/uploads', express.static('uploads'));
 app.use(express.json())
